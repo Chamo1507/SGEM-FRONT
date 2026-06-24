555232ad2ef161c01c436d48118b116f1851e805
Imanol Aguilar <imanolaguilare@gmail.com>
2026-06-23 21:59:17 -0600

    feature/CInput-added

---
+++ src/components/CInput.jsx
@@ -0,0 +1,49 @@
+import React, { forwardRef, useId } from 'react';
+import './CInput.css'; 
+
+const Input = forwardRef(({ 
+  label, 
+  error, 
+  id, 
+  className = '', 
+  type = 'text', 
+  ...props 
+}, ref) => {
+    
+  const generatedId = useId();
+  const inputId = id || generatedId;
+
+  return (
+    <div className={`input-container ${className}`}>
+      {/* Etiqueta (Label) */}
+      {label && (
+        <label 
+          htmlFor={inputId} 
+          className="input-label"
+        >
+          {label}
+        </label>
+      )}
+
+      {/* Campo de entrada */}
+      <input
+        id={inputId}
+        ref={ref}
+        type={type}
+        className={`input-field ${error ? 'has-error' : ''}`}
+        {...props}
+      />
+
+      {/* Mensaje de error */}
+      {error && (
+        <span className="input-error-text">
+          {error}
+        </span>
+      )}
+    </div>
+  );
+});
+
+Input.displayName = 'Input';
+
+export default Input;
